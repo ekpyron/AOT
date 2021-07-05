4370877ee206fb59f5ff2c576af499ed3ad85202
Daniel Kirchner <daniel@ekpyron.org>
2021-07-06 01:13:37 +0200

    More natural number test up to equinumerosity.

--- ThesisNaturalNumbers.thy
+++ ThesisNaturalNumbers.thy
@@ -7,7 +7,155 @@ begin
 chapter\<open>Natural Numbers in AOT\<close>
 
 text\<open>
+While AOT can represent mathematical theories including their deductive systems themselves
+as Abstract Objects, as mentioned in section (TODO: ref), it distinguishes this analysis of
+@{emph \<open>Theoretical Mathematics\<close>} from the notion of @{emph \<open>Natural Mathematics\<close>}. @{emph \<open>Natural\<close>}
+Mathematics consists of ordinary, pretheoretic claims about mathematical objects (TODO: cite PLM (303))
+and arise directly as abstraction of the exemplification patterns of ordinary objects rather
+than being based on the axioms of some mathematical theory.
 
+Following this idea, the claim of PLM's Chapter 14 (TODO: cite) is that natural numbers and Peano
+Arithmetic can be naturally defined within object theory and the laws they abide by up to and including
+Second-Order Peano Arithmetic can be derived without having to appeal to any intrinsically mathematical
+axioms or notions.
+
+We have reproduced parts of this construction in our implementation@{footnote \<open>At the
+time of writing the implementation encompasses the construction of natural numbers including
+mathematical induction. We expect a full derivation of Second-Order Peano Arithmetic in the
+foreseeable future.\<close>} and arrived at the following results:
+\<^item> The construction of natural numbers is sound and mathematical induction is derivable.
+\<^item> We could model the additional axioms required in the construction in our framework.
+\<^item> We could generalize one of the aforementioned axioms, strengthening the theoretical basis and
+  justification of the construction.
+\<^item> We could suggest several amendments to the construction and discover and fix several
+  minor errors and inconsistencies in the presentation.
+
+Interestingly, there are interactions between this construction and the paradox discovered
+in~\cite{Thesis} and discussed in TODO: cite session. We will describe this interaction in more
+detail in the following sections while reproducing the construction of Nodelman and Zalta and
+thereby show how our work towards amending object theory to overcome the paradox was a prerequisite
+for the current version of the construction.
+\<close>
+
+section\<open>General Idea of the Construction\<close>
+
+text\<open>
+The strategy for constructing natural numbers in AOT follows the idea of Frege's Theorem. (TODO: cite.)
+Frege showed that the Peano axioms can be derived from @{emph \<open>Hume's Principle\<close>} using Second-Order
+Logic. Hume's Principle states that the number of @{term F}s is equal to the number of @{term G}s if and
+only if @{term F} and @{term G} are @{emph \<open>equinumerous\<close>}. Two relations are @{emph \<open>equinumerous\<close>},
+if and only if there is a one-to-one correspondence between them or, in other words, if and only if
+there is a bijection between the objects exemplifying @{term F} and @{term G}. (TODO: add some citations).
+
+Frege himself derived Hume's Principle from @{emph \<open>Basic Law V\<close>}, which together with second-order
+logic leads to Russel's Paradox. However, deriving Peano arithmetic @{emph \<open>from\<close>} Hume's Principle
+itself does not require @{emph \<open>Basic Law V\<close>}. In Chapter 14 PLM, Nodelman and Zalta propose a
+definition of @{emph \<open>equinumerosity\<close>} and @{emph \<open>the number of @{term F}s\<close>} in object theory
+and derive Hume's Principle. Based on that Natural Numbers and Peano Arithmetic become consistently
+derivable as expected.
+\<close>
+
+section\<open>Equinumerosity of Relations\<close>
+
+text\<open>On the basis of traditional mathematical training based on set theory and functional logic,
+the seemingly most natural conception of @{emph \<open>equinumerosity\<close>} is based on the notion of a
+bijection. Two properties are equinumerous, respectively they count the same number of objects,
+if and only if there is a bijection between the sets of objects they exemplify.
+
+However, this conception of equinumerosity relies on objects of theoretical mathematics
+and their axiomatization (sets, functions, bijection). While object theory can in fact define
+those notions as well, it takes relations to be the more primitive, fundamental notion and thereby
+prefers a definition in terms of relations alone.
+
+The concept of there being a bijection between the sets of objects two properties exemplify can 
+equivalently be captured by the notion of there being a @{emph \<open>one-to-one correspondence\<close>} between
+the properties.
+
+A @{emph \<open>one-to-one correspondence\<close>} between the properties @{term F} and @{term G} is a relation
+@{term R}, s.t. (1) for every object @{term x} that exemplifies @{term F}, there is a unique object
+@{term y} exemplifying @{term G}, s.t. @{term x} bears @{term R} to @{term y} and conversely (2) for
+every object @{term y} that exemplifies @{term G}, there is a unique object @{term x} exemplifying
+@{term F}, s.t. @{term x} bears @{term R} to @{term y}. Formally:@{footnote \<open>Note that similar to
+the previous sections we are again directly quoting theorems verified in the Isabelle theory.\<close>}
+
+@{lemma[display] \<open>print_as_theorem \<guillemotleft>R |: F \<^sub>1\<^sub>-\<^sub>1\<longleftrightarrow> G \<equiv> \<forall>x ([F]x \<rightarrow> \<exists>!y ([G]y & [R]xy)) & \<forall>y ([G]y \<rightarrow> \<exists>!x ([F]x & [R]xy))\<guillemotright>\<close> by (auto dest: "&E" "1-1-cor"[THEN "\<equiv>\<^sub>d\<^sub>fE"] intro!: print_as_theoremI "\<equiv>I" "\<rightarrow>I" "&I" "cqt:2[const_var]"[axiom_inst] "1-1-cor"[THEN "\<equiv>\<^sub>d\<^sub>fI"])}
+
+However, this unrestricted notion of one-to-one correspondence is not well suited for a definition
+of equinumerosity that validates Hume's principle. The intuitive reason for this is that abstract
+objects cannot be counted. In particular, recall that there are distinct, but
+exemplification-indistinguishable abstract objects (TODO: make sure this is mentioned earlier):
+
+@{thm[display] "aclassical2"[print_as_theorem]}
+
+Based on this fact, we can prove that there is no one-to-one correspondence between @{term \<open>\<guillemotleft>A!\<guillemotright>\<close>}
+and itself:@{footnote \<open>We choose this opportunity to showcase that reasoning in object theory using
+our embedding in Isabelle is intuitively understandable. TODO: maybe add "once more"\<close>}
+\<close>
+
+AOT_theorem \<open>\<not>\<exists>R R |: A! \<^sub>1\<^sub>-\<^sub>1\<longleftrightarrow> A!\<close>
+proof(rule "raa-cor:2") \<comment> \<open>Proof by contradiction.\<close>
+  AOT_assume \<open>\<exists>R R |: A! \<^sub>1\<^sub>-\<^sub>1\<longleftrightarrow> A!\<close> \<comment> \<open>Assume the contrary.\<close>
+  then AOT_obtain R where 0: \<open>R |: A! \<^sub>1\<^sub>-\<^sub>1\<longleftrightarrow> A!\<close> \<comment> \<open>Let @{term R} be a witness.\<close>
+    using "\<exists>E" by metis 
+  \<comment> \<open>By definition of a one-to-one correspondence it follows that:\<close>
+  AOT_hence \<open>\<forall>x ([A!]x \<rightarrow> \<exists>!y ([A!]y & [R]xy))\<close>
+    using "1-1-cor"[THEN "\<equiv>\<^sub>d\<^sub>fE"] "&E" by blast+
+  \<comment> \<open>Now let @{term a} and @{term b} be witnesses to the theorem cited above.\<close>
+  moreover AOT_obtain a b where 1: \<open>A!a & A!b & a \<noteq> b & \<forall>F([F]a \<equiv> [F]b)\<close>
+    using "aclassical2" "\<exists>E"(*<*)[rotated](*>*) by blast
+  \<comment> \<open>Taken together, this means there has to be unique abstract object to which @{term a} bears @{term R}.\<close>
+  ultimately AOT_have \<open>\<exists>!y ([A!]y & [R]ay)\<close>
+    using "\<forall>E"(2) "&E" "\<rightarrow>E" by blast
+  \<comment> \<open>Now let @{term c} be a witness, s.t. @{term c} is abstract and @{term a} bears @{term R} to @{term c}.\<close>
+  then AOT_obtain c where 2: \<open>A!c & [R]ac\<close>
+    using "&E"(1) "\<exists>E"(*<*)[rotated](*>*) "uniqueness:1"[THEN "\<equiv>\<^sub>d\<^sub>fE"] by blast
+  \<comment> \<open>By beta-conversion it follows that @{term a} exemplifies @{emph \<open>being an @{term x} that bears @{term R} to @{term c}.\<close>}\<close>
+  AOT_have \<open>[\<lambda>x [R]xc]a\<close>
+  \<comment> \<open>Since by construction @{term a} and @{term b} exemplify the same properties, the same holds true for @{term b}.\<close>
+    by (rule "\<beta>\<leftarrow>C"; "cqt:2[lambda]"; simp add: "cqt:2[const_var]"[axiom_inst] 2[THEN "&E"(2)])
+  AOT_hence \<open>[\<lambda>x [R]xc]b\<close>
+    by (safe intro!: 1[THEN "&E"(2), THEN "\<forall>E"(1), THEN "\<equiv>E"(1)]) "cqt:2[lambda]"
+  \<comment> \<open>Again by beta conversion it follows that @{term b} bears @{term R} to @{term c}.\<close>
+  AOT_hence 5: \<open>[R]bc\<close>
+    using "\<beta>\<rightarrow>C" by blast
+  \<comment> \<open>Now the following is a consequence of @{term \<open>\<guillemotleft>A!\<guillemotright>\<close>} being in one-to-one correspondence to itself:\<close>
+  AOT_have \<open>\<forall>x \<forall>y \<forall>z ([A!]x & [A!]y & [A!]z \<rightarrow> ([R]xz & [R]yz \<rightarrow> x = y))\<close>
+    using "eq-1-1"[unvarify F G, OF "oa-exist:2", OF "oa-exist:2", THEN "\<equiv>E"(1),
+                   THEN "fFG:4"[THEN "\<equiv>\<^sub>d\<^sub>fE"], THEN "&E"(1),
+                   THEN "fFG:2"[THEN "\<equiv>\<^sub>d\<^sub>fE"], THEN "&E"(2), OF 0].
+  \<comment> \<open>In particular this is true for @{term a}, @{term b} and @{term c}.\<close>
+  AOT_hence \<open>[A!]a & [A!]b & [A!]c \<rightarrow> ([R]ac & [R]bc \<rightarrow> a = b)\<close>
+    using "\<forall>E"(2) by blast
+  \<comment> \<open>But we already established that @{term a}, @{term b} and @{term c} are abstract, as well as
+      that @{term a} bears @{term R} to @{term c} and @{term b} bears @{term R} to @{term c}, so
+      @{term a} and @{term b} have to be identical.\<close>
+  AOT_hence \<open>a = b\<close>
+    using 1[THEN "&E"(1)] 2 5 "&E" "\<rightarrow>E" "&I" by metis
+  \<comment> \<open>Which contradicts @{term a} and @{term b} being distinct by construction.\<close>
+  AOT_thus \<open>p & \<not>p\<close> for p
+    using 1 "=-infix" "\<equiv>\<^sub>d\<^sub>fE" "&I" "&E" "raa-cor:1" by fast
+qed
+
+text\<open>
+So if @{emph \<open>equinumerosity\<close>} was contingent on the existence of a full one-to-one correspondence,
+@{term \<open>\<guillemotleft>A!\<guillemotright>\<close>} would not be equinumerous to itself and consequently equinumerosity
+would not be an equivalence relation. However, Frege's Theorem does rely on equinumerosity being
+an equivalence relation. Fortunately, there is a natural solution to this issue.
+As mentioned in the introduction of this chapter, natural mathematics arises from abstracting
+patterns of @{emph \<open>ordinary\<close>} objects. And ordinary objects are what can naturally be counted.
+Hence Nodelman and Zalta introduce the notion of one-to-one correspondences among the ordinary
+objects. To that end, they introduce the restricted variables @{term u}, @{term v}, @{term r}, @{term s}
+that range over only the ordinary objects.@{footnote \<open>Recall the discussion about reproducing
+restricted variables in the embedding in: TODO.\<close>} Using these restricted variables, a one-to-one
+correspondence among the ordinary objects can be defined in the same way as a full one-to-one
+correspondence:
+
+@{lemma[display] \<open>print_as_theorem \<guillemotleft>R |: F \<^sub>1\<^sub>-\<^sub>1\<longleftrightarrow>\<^sub>E G \<equiv> \<forall>u ([F]u \<rightarrow> \<exists>!v ([G]v & [R]uv)) & \<forall>v ([G]v \<rightarrow> \<exists>!u ([F]u & [R]uv))\<guillemotright>\<close> by (auto dest: "&E" "equi:2"[THEN "\<equiv>\<^sub>d\<^sub>fE"] intro!: print_as_theoremI "\<equiv>I" "\<rightarrow>I" "&I" "cqt:2[const_var]"[axiom_inst] "equi:2"[THEN "\<equiv>\<^sub>d\<^sub>fI"])}
+
+Based on one-to-one correspondences on the ordinary objects, equinumerosity on the ordinary objects
+can be defined and can indeed shown to be an equivalence relation:
+
+@{thm[display] "equi:3"[of F G] "eq-part:1"[of _ F, print_as_theorem] "eq-part:2"[of _ F G, print_as_theorem] "eq-part:3"[of _ F G H, print_as_theorem]}
 \<close>
 
 chapter\<open>Higher-Order Type-Theoretic Object Theory\<close>
